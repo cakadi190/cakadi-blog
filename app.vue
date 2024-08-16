fbbd896bcc6d14377ad3183a9397004051ca4df0
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-08-16 10:18:02 +0700

    "Added Speed Insights component to App.vue and installed @vercel/speed-insights package"

--- app.vue
+++ app.vue
@@ -4,10 +4,14 @@
 			<NuxtLoadingIndicator />
 			<NuxtPage />
 		</NuxtLayout>
+
+    <SpeedInsights />
 	</div>
 </template>
 
 <script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+
 const { initDarkMode } = useDarkMode();
 const { initAll } = useBootstrapFive();
 const { getDyslexiaStyle, initDyslexiaFont } = useDyslexiaSettings();
