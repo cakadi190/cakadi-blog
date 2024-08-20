99d079969376f618f1d9b08ba5271e17a833006d
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-08-21 06:39:20 +0700

    "Updated footer layout and styling in footer-main.vue"

--- components/partials/global/footer-main.vue
+++ components/partials/global/footer-main.vue
@@ -10,22 +10,33 @@
     <section id="footer-top">
       <div class="container">
         <div class="row gy-4 gy-lg-0">
-          <div class="col-md-4">
+          <div class="col-md-3">
             <nuxt-link to="/" class="logo-wrapper mb-5">
               <nuxt-img :preload="true" format="webp" sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 800px"
               densities="x1 x2" alt="Logo Catatan Cak Adi" src="/images/brands/logo-white-long.svg" :width="48" height="auto" />
             </nuxt-link>
             
             <div class="d-flex justify-content-center justify-content-lg-start gap-2 flex-wrap">
-              <nuxt-link class="btn btn-light text-dark" to="/page/about-us">Tentang</nuxt-link>
-              <nuxt-link class="btn btn-light text-dark" to="/page/catatan-rilis">Catatan Rilis</nuxt-link>
-              <nuxt-link class="btn btn-light text-dark" to="/page/privacy-and-policy">Kebjakan Privasi</nuxt-link>
-              <nuxt-link class="btn btn-light text-dark" to="/page/comment-policy">Kebjakan Berkomentar</nuxt-link>
+              <nuxt-link class="btn btn-light text-dark btn-sm" to="/page/about-us">Tentang</nuxt-link>
+              <nuxt-link class="btn btn-light text-dark btn-sm" to="/page/catatan-rilis">Catatan Rilis</nuxt-link>
+              <nuxt-link class="btn btn-light text-dark btn-sm" to="/page/privacy-and-policy">Kebjakan Privasi</nuxt-link>
+              <nuxt-link class="btn btn-light text-dark btn-sm" to="/page/comment-policy">Kebjakan Berkomentar</nuxt-link>
             </div>
           </div>
-          <div class="col-md-4">
+          <div class="col-md-3">
+            <div class="mb-4">
+              <h3 class="text-merriweather">Hubungi</h3>
+              <p class="mb-0 mt-3"><a href="https://wa.me/6281333550746?text=Halo+Cak+Adi!" class="text-white text-decoration-none d-flex">(+62) 813-3355-0746</a></p>
+            </div>
+            <div class="mb-4">
+              <h3 class="text-merriweather">Surel</h3>
+              <p class="mb-0 mt-3"><a href="mailto:catatancakadi@gmail.com" class="text-white text-decoration-none d-flex">catatancakadi@gmail.com</a></p>
+            </div>
+          </div>
+          <div class="col-md-3"></div>
+          <div class="col-md-3">
             <div class="text-center rounded" id="card">
-              <h5 class="text-center">Berlangganan</h5>
+              <h5 class="text-center h3 text-merriweather mb-3">Berlangganan</h5>
               <p>Mau dapetin info soal koding, desain dan juga soal kepramukaan? Yuk berlangganan dan dapetin semua benefit yang
                 saya sebutkan tadi.</p>
           
@@ -93,11 +104,11 @@ const currentYear = new Date().getFullYear();
   .logo-wrapper {
     display: flex;
     align-items: center;
-    height: 40px;
+    height: 32px;
     width: auto;
     
     img {
-      height: 40px;
+      height: 32px;
       width: auto;
 
       @media screen and (max-width: 992px) {
